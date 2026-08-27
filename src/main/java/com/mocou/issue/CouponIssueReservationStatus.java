5643cb128a4d43dc97cd2b7c7fe82feb55ac5923
H0ng <ckdals7506@naver.com>
2026-08-27 13:24:30 +0900

    docs: 쿠폰 발급 API Swagger 상세 명세 추가

--- src/main/java/com/mocou/issue/CouponIssueReservationStatus.java
+++ src/main/java/com/mocou/issue/CouponIssueReservationStatus.java
@@ -1,5 +1,9 @@
 package com.mocou.issue;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "쿠폰 발급 예약 상태")
 public enum CouponIssueReservationStatus {
+    @Schema(description = "Redis 예약 성공 및 DB 비동기 저장 대기 상태")
     RESERVED
 }
